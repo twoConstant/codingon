bebfc0b9a5c39553f257878c24bbdfeca310bf27
newtkdtn <newtkdtn@gmail.com>
2024-12-18 12:03:48 +0900

    [FIX] 12_18 practice2

--- practice_12_17_1/Form4.cs
+++ practice_12_17_1/Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace practice_12_17_1
 {
@@ -15,18 +16,24 @@ namespace practice_12_17_1
         public Form4()
         {
             InitializeComponent();
+            textBox1.Text = "";
             string result = (isWin(true))? "승리" : "패배";
-            textBox1.Text = "" + result;
+            textBox1.Text += "동전 던지기 결과: " + result + "\r\n";
         }
 
         public bool isWin(bool myWish)
         {
+            int myWish_int = (myWish) ? 1 : 0;
+
             Random rand = new Random();
             int num_random = rand.Next();
             int divider = 2;
-            int result = num_random % divider;
+            int remaind = num_random % divider;
+            textBox1.Text += "내 배팅: " + myWish_int + "\r\n";
+            textBox1.Text += "난수: " + num_random + "\r\n";
+            textBox1.Text += "divider: " + divider + "\r\n";
 
-            return result == 1;
+            return (myWish_int != remaind)? false : true;
         }
     }
 }
