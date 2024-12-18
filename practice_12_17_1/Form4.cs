756d02d9f023dcfe740b5784c86654830c8bd1a2
newtkdtn <newtkdtn@gmail.com>
2024-12-18 11:45:14 +0900

    12_18 practice2 if sentence

---
+++ practice_12_17_1/Form4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice_12_17_1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            string result = (isWin(true))? "승리" : "패배";
+            textBox1.Text = "" + result;
+        }
+
+        public bool isWin(bool myWish)
+        {
+            Random rand = new Random();
+            int num_random = rand.Next();
+            int divider = 2;
+            int result = num_random % divider;
+
+            return result == 1;
+        }
+    }
+}
