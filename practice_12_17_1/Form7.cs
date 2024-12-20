8fc772c750bb5cabfd407535796f9e1d445f5c1d
newtkdtn <newtkdtn@gmail.com>
2024-12-20 10:59:09 +0900

    [REFACT] for practice

--- practice_12_17_1/Form7.cs
+++ practice_12_17_1/Form7.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace practice_12_17_1
 {
     public partial class Form7 : Form
     {
+        private readonly Random rnd = new Random();
+
         public Form7()
         {
             InitializeComponent();
@@ -24,19 +27,24 @@ namespace practice_12_17_1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string inputNumOfStudent_str = textBox1.Text;
-            int[] studentScore_arr = generateStudentScoreArr(int.Parse(inputNumOfStudent_str));
+            // 유효성 검사
+            if(!(int.TryParse(textBox1.Text, out int inputNumOfStudent) && inputNumOfStudent >= 0))
+            {
+                // 숫자가 아닌경우와 0 이하의 숫자를 입력한 경우
+                return;
+            }
+
+            int[] studentScore_arr = generateStudentScoreArr(inputNumOfStudent);
             string strToPrint = makeStrToPrint(studentScore_arr);
             textBox2.Text = strToPrint;
         }
 
         private int[] generateStudentScoreArr(int num)
         {
-            Random rnd = new Random();
             int[] scoreArr = new int[num];
             for(int i = 0; i < num; i++)
             {
-                scoreArr[i] = rnd.Next(0, 101);
+                scoreArr[i] = rnd.Next(0, 101);    // rnd : 멤버변수로 선언
             }
             return scoreArr;
         }
@@ -46,7 +54,7 @@ namespace practice_12_17_1
             StringBuilder sb = new StringBuilder();
             for(int i = 0; i < studentScore_arr.Length; i++)
             {
-                sb.Append("student" + i + " score: " + studentScore_arr[i] + "\r\n");
+                sb.Append($"student{i + 1} score: {studentScore_arr[i]} \r\n");
             }
             return sb.ToString();
         }
