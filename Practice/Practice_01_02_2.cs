1a292bac32871966efa7a3380cda83a6be16072a
newtkdtn <newtkdtn@gmail.com>
2025-01-02 13:20:21 +0900

    [Practice] try catch

---
+++ Practice/Practice_01_02_2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Practice_01_02_2
+    {
+        public static void Main(string[] args)
+        {
+            // 텍스트 파일의 컨탠츠를 불러온다.
+            string path = @"C:\projects\codingon\TextFile1.txt";
+            string errorPath = @"/dfjsl";
+            string[] contents;
+            TryReadTxtAndConvertToArray(errorPath, out contents);
+            PrintArray(contents);
+        }
+
+        // out을 이용해 파일 경로 및 배열을 입력, 파일의 내용을 각 줄마다 배열의 요소에 저장하는 메서드 작성
+        private static void TryReadTxtAndConvertToArray(string path, out string[] contents)
+        {
+            contents = new string[1];
+
+            try
+            {
+                string content = File.ReadAllText(path);
+                contents = content.Split("\r\n");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("파일이 존재하지 않습니다.");
+            }
+        }
+ 
+
+
+        private static void PrintArray(string[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+
+
+    }
+}
