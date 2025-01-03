439031a85608c951976e41bbf50ec3dddc878b40
newtkdtn <newtkdtn@gmail.com>
2025-01-03 12:16:36 +0900

    [PRACTIC] generic Dictionary

---
+++ practice_12_17_1/login.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace practice_12_17_1
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_filefinder = new System.Windows.Forms.Button();
+            this.button_login = new System.Windows.Forms.Button();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox_PW = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_filefinder
+            // 
+            this.button_filefinder.Location = new System.Drawing.Point(55, 79);
+            this.button_filefinder.Name = "button_filefinder";
+            this.button_filefinder.Size = new System.Drawing.Size(75, 23);
+            this.button_filefinder.TabIndex = 0;
+            this.button_filefinder.Text = "파일찾기";
+            this.button_filefinder.UseVisualStyleBackColor = true;
+            this.button_filefinder.Click += new System.EventHandler(this.button_filefinder_Click);
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(466, 338);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 1;
+            this.button_login.Text = "login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(306, 177);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(100, 21);
+            this.textBox_ID.TabIndex = 2;
+            this.textBox_ID.Text = "ID를 입력하세요";
+            // 
+            // textBox_PW
+            // 
+            this.textBox_PW.Location = new System.Drawing.Point(306, 230);
+            this.textBox_PW.Name = "textBox_PW";
+            this.textBox_PW.Size = new System.Drawing.Size(100, 21);
+            this.textBox_PW.TabIndex = 3;
+            this.textBox_PW.Text = "PW를 입력하세요";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PW:";
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_PW);
+            this.Controls.Add(this.textBox_ID);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.button_filefinder);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_filefinder;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox_PW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ practice_12_17_1/login.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace practice_12_17_1
+{
+    public partial class login : Form
+    {
+        private readonly Dictionary<string, string> pwDicsById = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> phonDicsById = new Dictionary<string, string>();
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_filefinder_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        Console.WriteLine($"fileContent: {fileContent}");
+                        FillDicsById(fileContent);
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+        private void FillDicsById(string fileContent)
+        {
+            string[] lines = fileContent.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"line: {line}"); // 디버깅용
+                string[] items = line.Split(',');
+
+                if (items.Length < 3)
+                {
+                    // ID와 비밀번호만 있는 경우, 전화번호를 빈 문자열로 처리
+                    string id = items[0];
+                    string pw = items[1];
+                    string phon = string.Empty;
+
+                    pwDicsById[id] = pw;
+                    phonDicsById[id] = phon;
+
+                    Console.WriteLine($"id: {id}, pw: {pw}, phon: {phon}"); // 디버깅용
+                    continue;
+                }
+
+                // ID, 비밀번호, 전화번호 모두 있는 경우
+                string idWithPhone = items[0];
+                string pwWithPhone = items[1];
+                string phonWithPhone = items[2];
+
+                pwDicsById[idWithPhone] = pwWithPhone;
+                phonDicsById[idWithPhone] = phonWithPhone;
+
+            }
+        }
+        
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string id = textBox_ID.Text;
+            string pw = textBox_PW.Text;
+
+            if (Authenticated(id, pw))
+            {
+                MessageBox.Show($"로그인 성공, ID: {id}, Phon: {phonDicsById[id]}");
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패");
+            }
+
+        }
+
+        private bool Authenticated(string id, string pw)
+        {
+            if(!pwDicsById.ContainsKey(id))
+            {
+                return false;
+            }
+
+            return pwDicsById[id] == pw;
+        }
+    }
+
+    
+}
